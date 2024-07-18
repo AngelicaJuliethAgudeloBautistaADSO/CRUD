2ca872c7b17fea27ce61aac6a0c7d34bd6fd83b8
angelica agudelo bautista <angelicaagudelobautista@gmail.com>
2024-07-18 06:59:36 -0500

    tercer commit

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 const $formulario = document.querySelector("form");
-
 const nombre = document.querySelector("#nombre");
 const apellidos = document.querySelector("#apellidos");
 const telefono = document.querySelector("#telefono");
 const direccion = document.querySelector("#direccion");
 const tipo = document.querySelector("#tipo");
 const documento = document.querySelector("#documento");
-
-const checkbox  = document.querySelector("#politicas");
-const summitBtn  = document.querySelector("#summit");
-
-nombre.addEventListener("blur", validarNombre);
-apellidos.addEventListener("blur", validarApellidos);
+const politicas = document.querySelector("#politicas");
+const boton = document.querySelector("#boton")
 
 const validar = () => {
     event.preventDefault()
@@ -62,30 +57,33 @@ const validar = () => {
 //quitar el borde rojo al momento que se ingresen los datos y la casilla deje de estar vacia
 const remover = (e, input) =>{
     if (input.value != "") {
-        input.classList.remove("error")
-        input.classList.add("correcto")
+        input.classList.remove("error");
+        input.classList.add("correcto");
+    }else{
+        input.classList.remove("correcto");
+        input.classList.add("error");
     }
 }
 $formulario.addEventListener("submit" , validar);
 
-nombre.addEventListener("blur", (event)=>{
-    remover(event, nombre)
-})
-apellidos.addEventListener("blur", (event)=>{
-    remover(event, apellidos)
-})
-telefono.addEventListener("blur", (event)=>{
-    remover(event, telefono)
-})
-direccion.addEventListener("blur", (event)=>{
-    remover(event, direccion)
-})
-tipo.addEventListener("blur", (event)=>{
-    remover(event, tipo)
-})
-documento.addEventListener("blur", (event)=>{
-    remover(event, documento)
-})
+nombre.addEventListener("blur", (event) => {
+    remover(event, nombre);
+});
+apellidos.addEventListener("blur", (event) => {
+    remover(event, apellidos);
+});
+telefono.addEventListener("blur", (event) => {
+    remover(event, telefono);
+});
+direccion.addEventListener("blur", (event) => {
+    remover(event, direccion);
+});
+tipo.addEventListener("blur", (event) => {
+    remover(event, tipo);
+});
+documento.addEventListener("blur", (event) => {
+    remover(event, documento);
+});
 // nombre.addEventListener("blur", function () {
 //     if (nombre.value === "") {
 //         nombre.classList.add("error")
@@ -94,43 +92,116 @@ documento.addEventListener("blur", (event)=>{
 //     }
 // })
 
-
 // $formulario.addEventListener("submit" , function(event){
 //     event.preventDefault();
 //     console.log(event)
 // })
 
-
-
-function validarNombre() {
-    const valorNombre = nombre.value.trim();
-    if (valorNombre === "" || valorNombre.length < 3 || !esTexto(valorNombre)) {
-      nombre.classList.add("error");
-      return false;
-    } else {
-      nombre.classList.remove("error");
-      return true;
+$formulario.addEventListener("submit", validar);
+nombre.addEventListener("keydown", (event) => {
+    remover(event, nombre);
+});
+apellidos.addEventListener("keydown", (event) => {
+    remover(event, apellidos);
+});
+telefono.addEventListener("keydown", (event) => {
+    remover(event, telefono);
+});
+direccion.addEventListener("keydown", (event) => {
+    remover(event, direccion);
+});
+tipo.addEventListener("keydown", (event) => {
+    remover(event, tipo);
+});
+documento.addEventListener("keydown", (event) => {
+    remover(event, documento);
+});
+//que solo resivan letras y caracteres especiales el nombre y apellido
+const letras = (event, elemento) =>{
+    let letras =/^[a-zA-ZáéíóúÁÉÍÓÚ\s]*$/;
+    // console.log(letras.test(elemento.value))
+    if (letras.test(event.key)) {
+        console.log("si")
+    }else{
+        console.log("#no")
     }
-  }
-  
-  function validarApellidos() {
-    const valorApellidos = apellidos.value.trim();
-    if (valorApellidos === "" || valorApellidos.length < 3 || !esTexto(valorApellidos)) {
-      apellidos.classList.add("error");
-      return false;
-    } else {
-      apellidos.classList.remove("error");
-      return true;
-    }
-  }
-  
-  function esTexto(valor) {
-    const soloTexto = /^[a-zA-Z\s]+$/;
-    return soloTexto.test(valor);
-  }
+}
+// const soloLetras = (event, elemento) => {
+//   let letras = /^[a-zA-ZáéíóúÁÉÍÓÚ\s]+$/;
+//   if (!letras.test(elemento.value + event.key)) {
+//     event.preventDefault();
+//   }
+// };
 
-politicas.addEventListener("change", () => {
-    summitBtn .disabled = !checkbox.checked;
+// nombre.addEventListener("input", (event) => {
+//   soloLetras(event, nombre);
+// });
+
+// apellidos.addEventListener("input", (event) => {
+//   soloLetras(event, apellidos);
+// });
+
+//boton enviar hasta que se acepten las politicas
+addEventListener("DOMContentLoaded",(event)=>{
+    //console.log(politicas.checked);
+    if(!politicas.checked){
+        // console.log(boton);
+        boton.setAttribute("disabled", "");
+    }
+});
+politicas.addEventListener("change", function(e){
+    console.log(e.target.checked);
+    if(e.target.checked){
+        boton.removeAttribute("disabled")
+    }
 });
 
-summitBtn .disabled = true;
+const numeros =(event) =>{
+    if (event.keyCode < 48 || event.keyCode > 57) {
+        event.preventDefault();
+        console.log("a");
+    }
+}
+// const letras = (event) =>{
+//     // if (event.keyCode < 97 || event.keyCode > 122 && event.keyCode === [250, 243, 237, 233, 225].includes(charCode)){
+//     //     event.preventDefault();
+//     let letras =/a-zA-Z/;
+//     if (letras.test) {
+//         console.log("si")
+//     }else{
+//         console.log("#no")
+//     }
+// }
+
+// //solo numeros en el telefono
+// telefono.addEventListener("keypress", function (event) {
+//     console.log(event.keyCode);  
+//     if (event.keyCode >= 48 && event.keyCode <= 57) {
+        
+//     }else{
+//         event.preventDefault()
+//     }
+// });
+
+// //solo numeros en el documento
+// // cuando pulsamos la tecla
+// documento.addEventListener("keypress", function (event) {
+//     console.log(event.keyCode);  
+//     if (event.keyCode >= 48 && event.keyCode <= 57) {
+        
+//     }else{
+//         event.preventDefault()
+//     }
+// });
+// // mientras mantegamos pulsada una letra
+// documento.addEventListener("keydowm", function (event) {
+//     // console.log("keydowm",event);
+// });
+// // cuando pulsamos la tecla
+// documento.addEventListener("keyup", function (event) {
+//     // console.log("keyup",event);
+// });
+documento.addEventListener("keypress", numeros)
+telefono.addEventListener("keypress", numeros)
+nombre.addEventListener("keypress", letras)
+apellidos.addEventListener("keypress", letras)
